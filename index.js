084490b031c680a474aa40fe74a236d6b2705fe4
Nazire <nazirej@gmail.com>
2022-09-14 08:29:29 -0400

    commit tree 659f6aa0a87c95bd80bbb6526c72a9e1fab98572

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// }
+
+const input = document. getElementById('button');
+
 function addingEventListener() {
+ input.addEventListener('click', function() {
+    return ('I was clicked')
+ });
 }
